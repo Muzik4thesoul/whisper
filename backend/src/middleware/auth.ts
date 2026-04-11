d921668c2bb155f5c9bc890d32b06c5cc38628ae
Vusimuzi Donald Junior Ndhlovu <vusideejunior@gmail.com>
2026-04-12 00:44:59 +0200

    fix: update error message for user not found in protectRoute middleware

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -18,7 +18,7 @@ export const protectRoute = [
           .json({ message: "Unauthorized - invalid token" });
 
       const user = await User.findOne({ clerkId });
-      if (!user) return res.status(404).json({ message: "User not found" });
+      if (!user) return res.status(404).json({ message: "User is not found" });
 
       req.userId = user._id.toString();
 
